7df7d3892a123c8dc2b3a2556a74c2f268c87ed7
Paulo Barcelos <paulo@azucrina.org>
2013-03-12 00:58:40 +0100

    Attachemtn

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
-<?php get_header(); ?>
+<?php get_template_part( 'page', 'header' ); ?>
 
 <?php while( have_posts() ): the_post(); ?>
+
+	<?php 
+		$embed = get_post_meta(get_the_ID(), 'embed');
+		
+		foreach ($embed as $code): if($code):
+	?>
+		<div class="embed">
+			<?php 
+				$oembed = wp_oembed_get($code);
+				if($oembed) echo $oembed;
+				else echo $code;
+			?>
+		</div>
+	<?php endif; endforeach; ?>
+
 	<?php the_post_thumbnail( 'large', array('class' => 'featured') ); ?>
 
 	<h1><?php the_title(); ?></h1>
 	<div class="content">
-		<?php echo wpautop($post->post_content); ?>
+		<?php the_content(); ?>
 	</div>
+
 <?php endwhile;?>
 
-<?php get_footer(); ?>
+<?php get_template_part( 'page', 'footer' ); ?>
